41edb7b7842ea5d15a9a10be529ac553958089a0
Abraham Kim <abeeeeekim@gmail.com>
2024-12-27 00:34:05 -0500

    footer

--- src/components/layout/FooterComponent.tsx
+++ src/components/layout/FooterComponent.tsx
@@ -5,48 +5,33 @@ import { Footer } from 'flowbite-react';
 const FooterComponent = () => {
   return (
     <Footer container>
-      <div className='w-full'>
-        <div className='grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1'>
-          <div className='w-1/5'>
-            <a href='/'>
-              <img
-                src='/images/logo.webp'
-                alt='Young and Company'
-              />
-            </a>
-          </div>
-          <div className='grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6'>
-            <div>
-              <Footer.Title title='about' />
-              <Footer.LinkGroup col>
-                <Footer.Link href='#'>Flowbite</Footer.Link>
-                <Footer.Link href='#'>Tailwind CSS</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title='Follow us' />
-              <Footer.LinkGroup col>
-                <Footer.Link href='#'>Github</Footer.Link>
-                <Footer.Link href='#'>Discord</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title='Legal' />
-              <Footer.LinkGroup col>
-                <Footer.Link href='#'>Privacy Policy</Footer.Link>
-                <Footer.Link href='#'>Terms &amp; Conditions</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-          </div>
+      <div className='w-full text-center'>
+        <div className='w-full justify-between sm:flex sm:items-center sm:justify-between flex flex-col'>
+          <p className='mb-12 text-xs text-gray-500'>
+            Any tax advice contained in this website (including any attachments)
+            is not intended or written to be used, and cannot be used, for the
+            purpose of (i) avoiding penalties imposed under the Internal Revenue
+            Code or applicable state or local tax law or (ii) promoting,
+            marketing, or recommending to another party any transaction or
+            matter addressed herein.
+          </p>
+          <Footer.LinkGroup>
+            <Footer.Link href='/our-company'>Our Company</Footer.Link>
+            <Footer.Link href='/guides'>Guides</Footer.Link>
+            <Footer.Link href='/contact-us'>Contact</Footer.Link>
+            <Footer.Link
+              href='http://youngcotax.clientportal.com/'
+              target='_blank'>
+              Login
+            </Footer.Link>
+          </Footer.LinkGroup>
         </div>
         <Footer.Divider />
-        <div className='w-full sm:flex sm:items-center sm:justify-between'>
-          <Footer.Copyright
-            href='#'
-            by='Young & Company'
-            year={2024}
-          />
-        </div>
+        <Footer.Copyright
+          href='/'
+          by='Young & Company'
+          year={2024}
+        />
       </div>
     </Footer>
   );
