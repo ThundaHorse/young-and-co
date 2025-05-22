d21eabb8b881c91456af5fce9418b4abef4ae408
Abraham Kim <abeeeeekim@gmail.com>
2025-05-21 21:01:36 -0400

    sitemap?

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import commonjs from 'vite-plugin-commonjs';
 import viteCompression from 'vite-plugin-compression';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    Sitemap({ hostname: 'https://youngcotax.com' }),
     commonjs(),
     viteCompression({
       algorithm: 'gzip',
